baaec7cb3c8d2b1631e19974deb749157f5df223
Andry <strawer@mail.ru>
2022-08-04 20:44:19 +0300

    first commit

---
+++ src/test/java/com/github/WikiTest.java
@@ -0,0 +1,41 @@
+package com.github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class WikiTest {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.holdBrowserOpen = true;
+         Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void wikiSearch () {
+     open("https://github.com/");
+     $("[name = q]").click();
+     $("[name = q]").setValue("selenide").pressEnter();
+     $$("ul.repo-list li").first().$("a").click();
+     $("#wiki-tab").click();
+     $(byText("Soft assertions")).click();
+     $("#js-repo-pjax-container").shouldHave(text("""
+             Using JUnit5 extend test class:
+             @ExtendWith({SoftAssertsExtension.class})
+             class Tests {
+               @Test
+               void test() {
+                 Configuration.assertionMode = SOFT;
+                 open("page.html");
+
+                 $("#first").should(visible).click();
+                 $("#second").should(visible).click();
+               }
+             }"""));
+    }
+}
